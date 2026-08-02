7e442b80f7f4c345b42e0dbf516f5eae72eda613
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 18:54:44 -0400

    Update rejoin.lua

--- src/exxploits/utility/rejoin.lua
+++ src/exxploits/utility/rejoin.lua
@@ -1,1 +1,22 @@
+-- ASPECTV4 | REJOIN
+-- ID: 41 | Type: Utility
+-- Rejoins current server
 
+local Core = _G.Aspect
+
+local Rejoin = {
+    enabled = false
+}
+
+local function toggle(state)
+    Rejoin.enabled = state
+    if state then
+        game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+        Rejoin.enabled = false
+    end
+end
+
+Core.RegisterExploit(41, "Rejoin", toggle, {})
+
+print("[ASPECTV4] Rejoin loaded (disabled)")
+return Rejoin
