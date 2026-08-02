14f891e17ab30b640f5aca8f2e261bd3fef26252
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 18:54:22 -0400

    Add disabler utility for property change detection
    Implemented a disabler utility to override GetPropertyChangedSignal detections.

--- src/exxploits/utility/disabler.lua
+++ src/exxploits/utility/disabler.lua
@@ -1,1 +1,33 @@
+-- ASPECTV4 | DISABLER
+-- ID: 39 | Type: Utility
+-- Disables GetPropertyChangedSignal detections
 
+local Core = _G.Aspect
+
+local Disabler = {
+    enabled = false
+}
+
+local function toggle(state)
+    Disabler.enabled = state
+end
+
+local function disableDetections()
+    if not Disabler.enabled then return end
+    
+    -- Override GetPropertyChangedSignal
+    local oldGPC = getmetatable(game).__index.GetPropertyChangedSignal
+    getmetatable(game).__index.GetPropertyChangedSignal = function(obj, prop)
+        if prop == "Network" or prop == "Character" then
+            return function() end
+        end
+        return oldGPC(obj, prop)
+    end
+end
+
+Core.RegisterExploit(39, "Disabler", toggle, {})
+
+Core.AddHook("Heartbeat", disableDetections)
+
+print("[ASPECTV4] Disabler loaded (disabled)")
+return Disabler
