a4544feb232721d4d57abf9fdfd8b23a698adeef
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 19:01:02 -0400

    Add FOV adjustment feature to camera
    Implemented FOV adjustment functionality for the camera.

--- src/exxploits/legit/fov.lua
+++ src/exxploits/legit/fov.lua
@@ -1,1 +1,26 @@
+-- ASPECTV4 | FOV
+-- ID: 66 | Type: Legit
+-- Adjusts camera FOV
 
+local Core = _G.Aspect
+
+local FOV = {
+    enabled = false,
+    value = 120
+}
+
+local function toggle(state)
+    FOV.enabled = state
+    if state then
+        workspace.CurrentCamera.FieldOfView = FOV.value
+    else
+        workspace.CurrentCamera.FieldOfView = 70
+    end
+end
+
+Core.RegisterExploit(66, "FOV", toggle, {
+    value = { type = "slider", min = 60, max = 180, default = 120 }
+})
+
+print("[ASPECTV4] FOV loaded (disabled)")
+return FOV
