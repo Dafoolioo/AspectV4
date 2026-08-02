61c46f2857dd1459d9af1a641ee67bdcb698e89a
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 19:02:47 -0400

    Add TimeChanger exploit to modify in-game time
    Implemented a time changer feature to modify in-game time.

--- src/exxploits/legit/timechanger.lua
+++ src/exxploits/legit/timechanger.lua
@@ -1,1 +1,26 @@
+-- ASPECTV4 | TIME CHANGER
+-- ID: 73 | Type: Legit
+-- Changes in-game time
 
+local Core = _G.Aspect
+
+local TimeChanger = {
+    enabled = false,
+    time = 12
+}
+
+local function toggle(state)
+    TimeChanger.enabled = state
+    if state then
+        game:GetService("Lighting").TimeOfDay = TimeChanger.time * 60
+    else
+        game:GetService("Lighting").TimeOfDay = 12 * 60
+    end
+end
+
+Core.RegisterExploit(73, "TimeChanger", toggle, {
+    time = { type = "slider", min = 0, max = 24, default = 12 }
+})
+
+print("[ASPECTV4] TimeChanger loaded (disabled)")
+return TimeChanger
