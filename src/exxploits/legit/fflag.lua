f90ee302d160336344d51e0ddc933f6d1820cff1
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 19:00:51 -0400

    Update fflag.lua

--- src/exxploits/legit/fflag.lua
+++ src/exxploits/legit/fflag.lua
@@ -1,1 +1,27 @@
+-- ASPECTV4 | FFLAG
+-- ID: 65 | Type: Legit
+-- FFlag editor
 
+local Core = _G.Aspect
+
+local FFlag = {
+    enabled = false,
+    settings = {}
+}
+
+local function toggle(state)
+    FFlag.enabled = state
+    if state then
+        -- Set FFlags
+        for k, v in pairs(FFlag.settings) do
+            game:GetService("FFlag"):Set(k, v)
+        end
+    end
+end
+
+Core.RegisterExploit(65, "FFlag", toggle, {
+    settings = { type = "list", default = {} }
+})
+
+print("[ASPECTV4] FFlag loaded (disabled)")
+return FFlag
