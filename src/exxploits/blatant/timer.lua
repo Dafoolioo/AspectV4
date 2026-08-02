82140577dfc9a76fd9c20a550571c13dc4ddaabc
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 18:50:49 -0400

    Implement timer exploit to modify game speed
    Added a timer exploit to change game speed using StepPhysics.

--- src/exxploits/blatant/timer.lua
+++ src/exxploits/blatant/timer.lua
@@ -1,1 +1,29 @@
+-- ASPECTV4 | TIMER
+-- ID: 25 | Type: Blatant
+-- Changes game speed using StepPhysics
 
+local Core = _G.Aspect
+
+local Timer = {
+    enabled = false,
+    speed = 1.5
+}
+
+local function toggle(state)
+    Timer.enabled = state
+    if state then
+        game:GetService("RunService").Stepped:Connect(function(dt)
+            if Timer.enabled then
+                -- Multiply time
+                game:GetService("RunService").Stepped:Wait(Timer.speed - 1)
+            end
+        end)
+    end
+end
+
+Core.RegisterExploit(25, "Timer", toggle, {
+    speed = { type = "slider", min = 0.1, max = 5.0, default = 1.5 }
+})
+
+print("[ASPECTV4] Timer loaded (disabled)")
+return Timer
