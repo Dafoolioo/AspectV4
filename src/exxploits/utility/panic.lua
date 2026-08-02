063e5da047a5b83db8c0ed230982ee996c2c616d
Dafolio <jaymmaddineni@gmail.com>
2026-08-02 18:54:32 -0400

    Update panic.lua

--- src/exxploits/utility/panic.lua
+++ src/exxploits/utility/panic.lua
@@ -1,1 +1,22 @@
+-- ASPECTV4 | PANIC
+-- ID: 40 | Type: Utility
+-- Disables all modules at once
 
+local Core = _G.Aspect
+
+local Panic = {
+    enabled = false
+}
+
+local function toggle(state)
+    Panic.enabled = state
+    if state then
+        Core.Panic()
+        Panic.enabled = false  -- Auto-disable after panic
+    end
+end
+
+Core.RegisterExploit(40, "Panic", toggle, {})
+
+print("[ASPECTV4] Panic loaded (disabled)")
+return Panic
